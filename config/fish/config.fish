d2e96ec4aef2285f63d8628371b290530151e891
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2019-01-28 17:36:33 +0900

    :+1:  Update fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,4 @@
-# Bootstrap installation of fisher
+# fish shell: bootstrap installation of fisher
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     set FISH_FUNC_DIR $XDG_CONFIG_HOME/fish/functions
@@ -10,7 +10,7 @@ if not functions -q fisher
     fish -c fisher
 end
 
-# pipenv and pyenv
+# Python: pipenv and pyenv
 set -x PYENV_ROOT $HOME/.pyenv
 set -x PIPENV_VENV_IN_PROJECT 1
 pyenv init - | source
