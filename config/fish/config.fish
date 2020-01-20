1806e587151ceab3158f3507c451a52dd2d2b77c
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-01-21 03:01:46 +0900

    Update config (fish)

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,18 +4,16 @@ set -x EDITOR vim
 # fish: aliases
 alias rm "rm -i"
 
-# tool: pipenv and pyenv
-set -x PYENV_ROOT $HOME/.pyenv
-set -x PIPENV_VENV_IN_PROJECT 1
-pyenv init - | source
+# tool: pyenv
+if type pyenv >/dev/null 2>&1
+    pyenv init - | source
+end
 
 # tool: am
-set -x AM_CACHE_PATH $HOME/.am
+set -x AM_CACHE_PATH $HOME/.config/am
 mkdir -p $AM_CACHE_PATH
 
-# tool: Java SE 8
-set -x JAVA_HOME (/usr/libexec/java_home -v "1.8")
-set -x PATH $JAVA_HOME/bin $PATH
-
-# tool: fls/fcd
-source /usr/local/etc/fcd.fish
+# tool: fls & fcd
+if type fls >/dev/null 2>&1
+    source /usr/local/etc/fcd.fish
+end
