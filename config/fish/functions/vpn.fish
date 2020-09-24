0d0b60eda3472de2cc488452cedb3661dfc1f4cd
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-09-24 23:15:08 +0900

    Add fish function (vpn)

---
+++ config/fish/functions/vpn.fish
@@ -0,0 +1,17 @@
+function vpn -d "Connect/disconnect Cisco VPN"
+  set -l vpn /opt/cisco/anyconnect/bin/vpn
+  set -l option $argv[1]
+
+  switch $option
+    case connect
+      read -s -p "echo 'Passward: '" password
+      printf $VPN_USER\n$password\ny | $vpn -s connect $VPN_HOST
+    case disconnect
+      $vpn disconnect
+    case status
+      $vpn state
+    case '*'
+      echo Invalid option: $option
+      return 1
+  end
+end
