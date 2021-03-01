ab05fe3f123af4fbef491c99cfdae17405349fe1
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2021-03-01 14:07:38 +0900

    Order settings alphabetically

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,9 +1,17 @@
-# rm
-if type -q gmv
-    mkdir -p $HOME/.Trash
-    alias rm "gmv --backup=numbered -t $HOME/.Trash"
-else
-    alias rm "rm -i"
+# am
+if type -q am
+    set -x AM_CACHE_PATH $HOME/.config/am
+    mkdir -p $AM_CACHE_PATH
+end
+
+# direnv
+if type -q direnv
+    direnv hook fish | source
+end
+
+# fcd
+if type -q fls
+    source /usr/local/etc/fcd.fish
 end
 
 # ls
@@ -16,37 +24,28 @@ if test -f $HOME/.pythonrc
     set -x PYTHONSTARTUP $HOME/.pythonrc
 end
 
-# python: pyenv
-if type -q pyenv
-    pyenv init - | source
-end
-
-# python: direnv
-if type -q direnv
-    direnv hook fish | source
-end
-
-# python: poetry
-if type -q poetry
-    set -x POETRY_VIRTUALENVS_IN_PROJECT 1
-    alias run "poetry run"
-end
-
 # python: pipenv
 if type -q pipenv
     set -x PIPENV_VENV_IN_PROJECT 1
     set -x PIPENV_VERBOSITY -1
 end
 
-# am
-if type -q am
-    set -x AM_CACHE_PATH $HOME/.config/am
-    mkdir -p $AM_CACHE_PATH
+# python: poetry
+if type -q poetry
+    set -x POETRY_VIRTUALENVS_IN_PROJECT 1
 end
 
-# fcd
-if type -q fls
-    source /usr/local/etc/fcd.fish
+# python: pyenv
+if type -q pyenv
+    pyenv init - | source
+end
+
+# rm
+if type -q gmv
+    alias rm "gmv --backup=numbered -t $HOME/.Trash"
+    mkdir -p $HOME/.Trash
+else
+    alias rm "rm -i"
 end
 
 # vim
